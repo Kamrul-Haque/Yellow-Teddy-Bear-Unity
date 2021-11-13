188ecdcda9fb951d3742bb470fd72125fd609ef0
Kamrul-Haque <kamrul35-255@diu.edu.bd>
2021-11-13 21:28:31 +0600

    Backing Up

---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// makes game object larger
+/// </summary>
+public class Scaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 newScale = transform.localScale;
+        newScale.x *= 3;
+        newScale.y *= 4;
+        transform.localScale = newScale;
+    }
+}
---
+++ Assets/Scripts/TeddyBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeddyBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        const float MinForce = 3f;
+        const float MaxForce = 5f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+
+        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinForce, MaxForce);
+
+        GetComponent<Rigidbody2D>().AddForce(magnitude * direction, ForceMode2D.Impulse);
+    }
+}
